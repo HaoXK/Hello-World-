2ef690713e2b5938c9116b3eac802d19d182c1c8
HaoXK <haoxuank@nycstudents.net>
2024-09-09 22:30:59 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+//1.1 Hello World Program
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        System.out.print("Hi!");
+    }
+}
